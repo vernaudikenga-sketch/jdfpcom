1549706aca2a6b3f00216535759b9958653ab4eb
vernaudpelinda-art <vernaudpelinda@gmail.com>
2026-05-27 18:44:59 +0300

    Updated app-build-manifest.json

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"anKlnF/Ib/ZNmHuWt3pg9ot1SxPFsBxi1j769N5CwPk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gvnld3tvy1CvXTDpKD6YtajvPUeAcS4Uo4xTDJgTmD4=\"\n}"
